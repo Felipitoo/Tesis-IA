5a6a83ecc51e7cacc8894d227f766a6d0947b4b7
Felipe Montero Concha <felipe.montero.cl@gmail.com>
2021-09-10 15:43:31 -0300

    lectura del archivo completo

--- code/Instance.cpp
+++ code/Instance.cpp
@@ -32,21 +32,48 @@ std::vector<std::vector<float>> createDamagesMatrix(int rows, int columns){
   return matrix;
 }
 
-void print_matrix(std::vector<std::vector<int>> matrix){
+// void print_matrix(std::vector<std::vector<int>> matrix){
+//      for(int i = 0 ; i < matrix.size(); i++){
+//          for(int j = 0 ; j< matrix[0].size();j++){
+//              std::cout << matrix[i][j] << " ";
+//          }
+//          std::cout << "\n";
+//      }
+// }
+
+// // Funcion para imprimir un vector de valores enteros
+// void print_vector(std::vector<int> vect){
+//     for(int i = 0 ; i < vect.size(); i++){
+//         std::cout << vect[i] << " ";
+//         std::cout << "\n";
+//     }
+// }
+
+// // Funcion para imprimir un vector de valores enteros
+// void print_vector_text(std::vector<std::string> vect){
+//     for(int i = 0 ; i < vect.size(); i++){
+//         std::cout << vect[i] << " ";
+//     }
+//     std::cout << "\n";
+// }
+
+// Funcion para imprimir un vector de valores enteros
+template<typename T>
+void print_vector_gen(std::vector<T> vect){
+    for(int i = 0 ; i < vect.size(); i++){
+        std::cout << vect[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+template<typename T>
+void print_matrix(std::vector<std::vector<T>> matrix){
      for(int i = 0 ; i < matrix.size(); i++){
          for(int j = 0 ; j< matrix[0].size();j++){
              std::cout << matrix[i][j] << " ";
          }
          std::cout << "\n";
      }
-}
-
-// Funcion para imprimir un vector de valores enteros
-void print_vector(std::vector<int> vect){
-    for(int i = 0 ; i < vect.size(); i++){
-        std::cout << vect[i] << " ";
-        std::cout << "\n";
-    }
 }
 
 
@@ -63,6 +90,7 @@ Instance::Instance(std::string filename){
   while(std::getline(infile, mytext)){
       if (i == 1){
         split(mytext, words, ' ');
+        std::cout << mytext << '\n';
         this->dimension = std::stoi(words[1]);
         this->costMatrix = createEmptyMatrix(this->dimension, this->dimension);
         this->stateMatrix = createEmptyMatrix(this->dimension, this->dimension);
@@ -83,14 +111,11 @@ Instance::Instance(std::string filename){
       else if (mytext == "EDGE_WEIGHT_SECTION"){
         EWS = true;
       }
-      else if (EWS && mytext != "TYPE_OF_ARC"){
-        std::cout << i;
-        // std::cout << mytext;
+      else if ( EWS == true && mytext != "TYPE_OF_ARC"){
         split(mytext, words, ' ');
         int column= 0;
         for(std::string costo: words){
-          //std::cout << costo;
-          //this->costMatrix[k][column] = std::stoi(costo);
+          this->costMatrix[k][column] = std::stoi(costo);
           column++;      
         }
         k++;
@@ -101,70 +126,84 @@ Instance::Instance(std::string filename){
         TOA = true;
         k = 0;
       }
-      // else if (TOA && mytext != "CONDITION_OF_ARC"){
-      //   split(mytext, words, ' ');
-      //   int column= 0;
-      //   for(std::string costo: words){
-      //     this->typeMatrix[k][column] = std::stoi(costo);
-      //     column++;      
-      //   }
-      //   k++;
-      // }
-      // else if( mytext == "CONDITION_OF_ARC"){
-      //   TOA = false;
-      //   COA = true;
-      //   k = 0;
-      // }
-      // else if (COA && mytext != "DISTANCE"){
-      //   split(mytext, words, ' ');
-      //   int column= 0;
-      //   for(std::string costo: words){
-      //     this->stateMatrix[k][column] = std::stoi(costo);
-      //     column++;      
-      //   }
-      //   k++;
-      // }
-      // else if( mytext == "DISTANCE"){
-      //   D = true;
-      //   COA = false;
-      //   k = 0;
-      // }
-      // else if (D && mytext != "DAMAGES"){
-      //   split(mytext, words, ' ');
-      //   int column= 0;
-      //   for(std::string costo: words){
-      //     this->stateMatrix[k][column] = std::stoi(costo);
-      //     column++;      
-      //   }
-      //   k++;
-      // }
-      // else if( mytext == "DAMAGES"){
-      //   D = false;
-      //   DAM = true;
-      //   k = 0;
-      // }
-      // else if (DAM && mytext != "DEMAND_SECTION"){
-      //   split(mytext, words, ' ');
-      //   int column= 0;
-      //   for(std::string costo: words){
-      //     this->damages[k][column] = std::stof(costo);
-      //     column++;      
-      //   }
-      //   k++;
-      // }
-      // else if(mytext == "DEMAND_SECTION"){
-      //   DAM = false;
-      // }
-      // else{
-      //   split(mytext, words, ' ');
-      //   int id = std::stoi(words[0]) - 1;
-      //   int demand = std::stoi(words[1]);
-      //   Node auxNode = Node(id, demand);
-      //   this->nodes.push_back(auxNode);
-      // }
+      else if (TOA == true && mytext != "CONDITION_OF_ARC"){
+        split(mytext, words, ' ');
+        // std::cout << "Linea  " << mytext << '\n';
+        int column= 0;
+        for(std::string costo: words){
+          // std::cout << "Type  " << std::stoi(costo) << '\n';
+          // std::cout << k << "  " << column << '\n';
+          this->typeMatrix[k][column] = std::stoi(costo);
+          column++;      
+        }
+        k++;
+        words.clear();
+      }
+      else if( mytext == "CONDITION_OF_ARC"){
+        TOA = false;
+        COA = true;
+        k = 0;
+      }
+      else if (COA == true && mytext != "DISTANCE"){
+        split(mytext, words, ' ');
+        //std::cout << "Linea  " << mytext << '\n';
+        int column= 0;
+        for(std::string costo: words){
+          //std::cout << "Type  " << std::stoi(costo) << '\n';
+          // std::cout << k << "  " << column << '\n';
+          this->stateMatrix[k][column] = std::stoi(costo);
+          column++;      
+        }
+        k++;
+        words.clear();
+      }
+      else if( mytext == "DISTANCE"){
+        D = true;
+        COA = false;
+        k = 0;
+      }
+      else if (D == true && mytext != "DAMAGES"){
+        split(mytext, words, ' ');
+        int column= 0;
+        for(std::string costo: words){
+          this->stateMatrix[k][column] = std::stoi(costo);
+          column++;      
+        }
+        k++;
+        words.clear();
+      }
+      else if( mytext == "DAMAGES"){
+        D = false;
+        DAM = true;
+        k = 0;
+      }
+      else if (DAM == true && mytext != "DEMAND_SECTION"){
+        split(mytext, words, ' ');
+        int column= 0;
+        for(std::string costo: words){
+          this->damages[k][column] = std::stof(costo);
+          column++;      
+        }
+        k++;
+        words.clear();
+      }
+      else if(mytext == "DEMAND_SECTION"){
+        DAM = false;
+      }
+      else{
+        if(mytext != "TYPE GCVRP"){
+          words.clear();
+          split(mytext, words, ' ');
+          print_vector_gen(words);
+          int id = std::stoi(words[0]) - 1;
+          int demand = std::stoi(words[1]);
+          Node auxNode = Node(id, demand);
+          this->nodes.push_back(auxNode);
+        }
+      }
       i++;
   }
-  print_matrix(this->typeMatrix);
+  print_matrix(this->damages);
 
 
 }
